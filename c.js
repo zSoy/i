5002cd5276bb9b40d6b01b48b6b89a31e173caae
zSoy <58632519+zSoy@users.noreply.github.com>
2021-12-10 14:01:42 +0800

    Create c.js

---
+++ c.js
@@ -0,0 +1,23 @@
+(function(){
+  Element.prototype.on=Element.prototype.addEventListener;
+  NodeList.prototype.on=function(e,f){
+    []['forEach'].call(this,function(el){
+      el.on(e,f);
+    })
+  };
+  Element.prototype.trigger = function (type, data) {
+    var event = document.createEvent('HTMLEvents');
+    event.initEvent(type, true, true);
+    event.data = data || {};
+    event.eventName = type;
+    event.target = this;
+    this.dispatchEvent(event);
+    return this;
+  };
+  NodeList.prototype.trigger = function (event) {
+    []['forEach'].call(this, function (el) {
+      el['trigger'](event);
+    });
+    return this;
+  };
+})()
