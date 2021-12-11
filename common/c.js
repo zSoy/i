cb6a8a65d67c84dd4b25732158c21d2752e96bdd
zSoy <58632519+zSoy@users.noreply.github.com>
2021-12-11 22:15:12 +0800

    Update c.js

--- common/c.js
+++ common/c.js
@@ -1,5 +1,12 @@
 ;(function(){
   if(window.top!==window.self){window.top.location = window.location;};
+  window.onkeydown = e => {
+    switch(e.keyCode){
+      case 9:case 37:case 38:case 39:case 40:case 116:case 117: return true;
+      default:e.returnValue=0;
+        return false;
+    }
+  }
   Element.prototype.on=Element.prototype.addEventListener;
   NodeList.prototype.on=function(e,f){
     []['forEach'].call(this,function(el){
